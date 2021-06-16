2eb23dad4974898cf63a6e889dc71c14587f8658
sita ram kalluri <kalluriramkumar@gmail.com>
2021-06-16 18:49:52 +0530

    Move atlookupimpl.close into whenComplete block

--- at_server_status/lib/at_status_impl.dart
+++ at_server_status/lib/at_status_impl.dart
@@ -123,9 +123,7 @@ class AtStatusImpl implements AtServerStatus {
         // @server has root location, is not running and is not activated
         atStatus.serverStatus = ServerStatus.unavailable;
         print('_getServerStatus error: $error');
-        await atLookupImpl.close();
-      });
-      await atLookupImpl.close();
+      }).whenComplete(() async => await atLookupImpl.close());
     }
     return atStatus;
   }
