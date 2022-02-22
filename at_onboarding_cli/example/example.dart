0bc7eb97f664c3f8dcd0058d7abdb7ad5ac7f4a4
srieteja <srit1505@gmail.com>
2022-02-23 03:12:03 +0530

    minor modifications

--- at_onboarding_cli/example/example.dart
+++ at_onboarding_cli/example/example.dart
@@ -1,29 +1,23 @@
-import 'dart:io';
-
-import 'package:at_client/at_client.dart';
-import 'package:at_commons/at_commons.dart';
 import 'package:at_lookup/at_lookup.dart';
 import 'package:at_onboarding_cli/at_onboarding_cli.dart';
 
 void main() async {
+  //onboarding preference builder can be used to set onboardingService parameters
   AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
-    //..qrCodePath = <path to qr code containing cram secret>
-    ..qrCodePath = '/home/srie/Desktop/work/at_libraries/at_libraries/at_onboarding_cli/lib/config/qrcode_amateur14.png'
-    ..rootDomain = 'root.atsign.org'
-    ..rootPort = 64
-    ..hiveStoragePath = 'lib/config/storage'
-    ..namespace = 'test'
-    ..downloadPath = '/home/srie/Desktop/test_cli'
-    ..isLocalStoreRequired = true
-    ..commitLogPath = 'lib/config/commitLog'
-    //..cramSecret = <your cram secret>;
-    ;//..atKeysFilePath = 'lib/config/@capitalistfriedchicken_key.atKeys';
+        //..qrCodePath = <path to qr code containing cram secret>
+        ..rootDomain = 'root.atsign.org'
+        ..rootPort = 64
+        ..hiveStoragePath = 'lib/config/storage'
+        ..namespace = 'example'
+        ..downloadPath = '/home/srie/Desktop/test_cli'
+        ..isLocalStoreRequired = true
+        ..commitLogPath = 'lib/config/commitLog';
+        //..cramSecret = <your cram secret>;
+        //..atKeysFilePath = <path to .atKeysFile>
 
   AtOnboardingService onboardingService =
-      AtOnboardingServiceImpl('@amateur14', atOnboardingConfig);
+      AtOnboardingServiceImpl('your atsign here', atOnboardingConfig);
   await onboardingService.onboard();
   AtLookUp? atLookup = onboardingService.getAtLookup();
   print(await atLookup?.scan(regex: 'publickey'));
-  //print(await atLookup?.lookup('public:publickey'));
-
 }
