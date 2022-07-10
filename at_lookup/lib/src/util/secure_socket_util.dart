f5a239d7b6a8833ef6c1621cdead9469480e2d71
Srie Teja <srit1505@gmail.com>
2022-07-11 01:56:05 +0530

    modified secure_socket_util

---
+++ at_lookup/lib/src/util/secure_socket_util.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+class SecureSocketUtil {
+  ///method that creates and returns a [SecureSocket]. If [decryptPackets] is set to true,the TLS keys are logged into a file.
+  static Future<SecureSocket> createSecureContext(String host, String port,
+      bool decryptPackets, String? pathToCerts, String? tlsKeysSavePath) async {
+    if (decryptPackets) {
+      SecurityContext securityContext = SecurityContext();
+      File keysFile = File(tlsKeysSavePath!);
+      securityContext.setTrustedCertificates(pathToCerts!);
+      return await SecureSocket.connect(host, int.parse(port),
+          context: securityContext,
+          keyLog: (line) =>
+              keysFile.writeAsStringSync(line, mode: FileMode.append));
+    } else {
+      return await SecureSocket.connect(host, int.parse(port));
+    }
+  }
+}
