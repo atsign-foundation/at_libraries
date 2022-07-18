4df15ea095acc6afe854afff49611d9022e0226a
srieteja <srit1505@gmail.com>
2022-07-18 18:37:48 +0530

    error handling - secure_socket_util

--- at_lookup/lib/src/util/secure_socket_util.dart
+++ at_lookup/lib/src/util/secure_socket_util.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'package:at_commons/at_commons.dart';
 
 class SecureSocketUtil {
-  static bool decryptPackets = false;
+  static late bool decryptPackets;
 
   ///method that creates and returns a [SecureSocket]. If [decryptPackets] is set to true,the TLS keys are logged into a file.
   static Future<SecureSocket> createSecureSocket(
@@ -13,12 +14,16 @@ class SecureSocketUtil {
     SecureSocketUtil.decryptPackets = decryptPackets ?? false;
     if (SecureSocketUtil.decryptPackets) {
       SecurityContext securityContext = SecurityContext();
-      File keysFile = File(tlsKeysSavePath!);
-      securityContext.setTrustedCertificates(pathToCerts!);
-      return await SecureSocket.connect(host, int.parse(port),
-          context: securityContext,
-          keyLog: (line) =>
-              keysFile.writeAsStringSync(line, mode: FileMode.append));
+      try {
+        File keysFile = File(tlsKeysSavePath!);
+        securityContext.setTrustedCertificates(pathToCerts!);
+        return await SecureSocket.connect(host, int.parse(port),
+            context: securityContext,
+            keyLog: (line) =>
+                keysFile.writeAsStringSync(line, mode: FileMode.append));
+      } catch (e) {
+        throw AtException(e.toString());
+      }
     } else {
       return await SecureSocket.connect(host, int.parse(port));
     }
