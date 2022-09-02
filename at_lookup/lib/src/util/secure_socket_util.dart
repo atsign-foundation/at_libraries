f3a1430767ed7b4e9276bf6bec0f1d0607c78287
srieteja <srit1505@gmail.com>
2022-09-02 16:00:22 +0530

    setting socketOption tcpNoDelay -> true

--- at_lookup/lib/src/util/secure_socket_util.dart
+++ at_lookup/lib/src/util/secure_socket_util.dart
@@ -5,23 +5,29 @@ class SecureSocketUtil {
   ///method that creates and returns a [SecureSocket]. If [decryptPackets] is set to true,the TLS keys are logged into a file.
   static Future<SecureSocket> createSecureSocket(
       String host, String port, SecureSocketConfig secureSocketConfig) async {
+    SecureSocket? _secureSocket;
     if (!secureSocketConfig.decryptPackets) {
-      return await SecureSocket.connect(host, int.parse(port));
+      _secureSocket = await SecureSocket.connect(host, int.parse(port));
+      _secureSocket.setOption(SocketOption.tcpNoDelay, true);
+      return _secureSocket;
     } else {
       SecurityContext securityContext = SecurityContext();
       try {
         File keysFile = File(secureSocketConfig.tlsKeysSavePath!);
-        if (secureSocketConfig.pathToCerts != null && await File(secureSocketConfig.pathToCerts!).exists()) {
+        if (secureSocketConfig.pathToCerts != null &&
+            await File(secureSocketConfig.pathToCerts!).exists()) {
           securityContext
               .setTrustedCertificates(secureSocketConfig.pathToCerts!);
         } else {
           throw AtException(
               'decryptPackets set to true but path to trusted certificated not provided');
         }
-        return await SecureSocket.connect(host, int.parse(port),
+        _secureSocket = await SecureSocket.connect(host, int.parse(port),
             context: securityContext,
             keyLog: (line) =>
                 keysFile.writeAsStringSync(line, mode: FileMode.append));
+        _secureSocket.setOption(SocketOption.tcpNoDelay, true);
+        return _secureSocket;
       } catch (e) {
         throw AtException(e.toString());
       }
