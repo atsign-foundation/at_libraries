3825da58d1e8ef5fccf9171cbaa39e13edde5d5f
srieteja <50960424+srieteja@users.noreply.github.com>
2022-10-06 06:15:34 +0530

    fix register_cli path in exports file

--- at_onboarding_cli/lib/at_onboarding_cli.dart
+++ at_onboarding_cli/lib/at_onboarding_cli.dart
@@ -4,5 +4,5 @@ export 'src/onboard/at_onboarding_service_impl.dart';
 export 'src/onboard/at_onboarding_service.dart';
 export 'src/util/at_onboarding_preference.dart';
 export 'src/util/auth_key_type.dart';
-export 'src/register/register.dart';
+export 'src/register_cli/register.dart';
 export 'src/util/register_util.dart';
