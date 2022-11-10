d7f995354b9816986aa8d18c6bb769a3b1d7753b
murali-shris <murali.25@gmail.com>
2022-11-10 13:17:04 +0530

    rename method in at_chops_base

--- packages/at_chops/lib/src/at_chops_base.dart
+++ packages/at_chops/lib/src/at_chops_base.dart
@@ -3,9 +3,9 @@ import 'dart:typed_data';
 import 'package:at_chops/src/algorithm/at_algorithm.dart';
 
 abstract class AtChops {
-  List<int> encrypt(Uint8List data, AtEncryptionAlgorithm encryptionAlgorithm);
+  List<int> encryptBytes(Uint8List data, AtEncryptionAlgorithm encryptionAlgorithm);
   String encryptString(String data, AtEncryptionAlgorithm encryptionAlgorithm);
-  List<int> decrypt(Uint8List data, AtEncryptionAlgorithm encryptionAlgorithm);
+  List<int> decryptBytes(Uint8List data, AtEncryptionAlgorithm encryptionAlgorithm);
   String decryptString(String data, AtEncryptionAlgorithm encryptionAlgorithm);
   Uint8List sign(Uint8List data, AtSigningAlgorithm signingAlgorithm);
   bool verify(Uint8List signedData, Uint8List signature,
