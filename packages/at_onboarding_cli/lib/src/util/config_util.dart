67d3c9de63ad7007a64c5e31e31d95121df6fc17
srieteja <srit1505@gmail.com>
2022-11-15 15:58:17 +0530

    introduce config_util

---
+++ packages/at_onboarding_cli/lib/src/util/config_util.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+import 'package:at_client/at_client.dart';
+import 'package:path/path.dart' as path;
+
+class ConfigUtil {
+  static String? getHomeDirectory() {
+    switch (Platform.operatingSystem) {
+      case 'linux':
+      case 'macos':
+        return Platform.environment['HOME'];
+      case 'windows':
+        return Platform.environment['USERPROFILE'];
+      case 'android':
+        // Probably want internal storage.
+        return '/storage/sdcard0';
+      default:
+        return null;
+    }
+  }
+
+  static String getAtKeysPath(String atsign) {
+    final homeDir = getHomeDirectory();
+     if(homeDir == null){
+       throw AtClientException.message('Could not find home directory');
+     }
+     return path.join(homeDir, '.atsign', 'at_onboarding_cli', 'keys', '${atsign}_key.atKeys');
+  }
+
+  static String getStorageDirectory(String atsign){
+    final homeDir = getHomeDirectory();
+    if(homeDir == null){
+      throw AtClientException.message('Could not find home directory');
+    }
+    return path.join(homeDir, '.atsign', 'at_onboarding_cli', 'storage', atsign);
+  }
+
+  static String getCommitLogPath(String atsign){
+    return path.join(getStorageDirectory(atsign), 'commitLog');
+  }
+
+  static String getHiveStoragePath(String atsign){
+    return path.join(ConfigUtil.getStorageDirectory(atsign),'hive');
+  }
+}
