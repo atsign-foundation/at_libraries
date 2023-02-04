a4c5650a19f61a905a7c0d834f3b443f63866ef8
gkc <gary.casey@gmail.com>
2023-02-04 17:28:28 +0000

    feat: Added new encryption metadata to the `notify` verb syntax refactor: Some more tidying up of regexes in syntax.dart for readability and maintainability feat: Added named regex group `publicScope` to match 'public' in the appropriate places feat: Modified VerbUtil.getVerbParam so it sets the special 'isPublic' parameter to 'true' if the `publicScope` group has matched and the matched value is 'public'. This will enable elimination of code like this in various server-side verb handlers   ```     if (command.contains('public:')) {       verbParams.putIfAbsent('isPublic', () => 'true');     }   ```  refactor: In UpdateVerbBuilder.getBuilder, replaced `builder.isPublic = command.contains('public:');` with `builder.isPublic = verbParams[IS_PUBLIC] == 'true';`  test: Added some additional round-trip tests for UpdateVerbBuilder  test: Added round-trip assertions to some NotifyVerbBuilder tests  feat: Simplified and clarified regex for update and delete verbs by being more explicit in the handling of the very small number of exceptions  feat: Added toAtProtocolFragment method to Metadata  refactor: UpdateVerbBuilder and NotifyVerbBuilder to reuse common code fragments (especially Metadata.toAtProtocolFragment), eliminate redundant and/or duplicated instance variables for metadata attributes etc

--- lib/src/verb/update_verb_builder.dart
+++ lib/src/verb/update_verb_builder.dart
@@ -2,8 +2,7 @@ import 'dart:collection';
 import 'dart:convert';
 
 import 'package:at_commons/at_commons.dart';
-import 'package:at_commons/src/verb/abstract_verb_builder.dart';
-import 'package:meta/meta.dart';
+import 'package:at_commons/src/verb/metadata_using_verb_builder.dart';
 
 /// Update builder generates a command to update [value] for a key [atKey] in the secondary server of [sharedBy].
 /// Use [getBuilder] method if you want to convert command to a builder.
@@ -40,28 +39,10 @@ import 'package:meta/meta.dart';
 ///                      ..sharedBy = '@bob'
 ///                      ..value = jsonEncode(myPrefObj)
 ///```
-class UpdateVerbBuilder extends AbstractVerbBuilder {
-  UpdateVerbBuilder() {
-    atKeyObj.metadata!.isBinary = null;
-  }
+class UpdateVerbBuilder extends MetadataUsingVerbBuilder {
   /// Value of the key typically in string format. Images, files, etc.,
   /// must be converted to unicode string before storing.
   dynamic value;
-
-  /// See [AtKey.key]
-  String? get atKey => atKeyObj.key;
-  /// See [AtKey.key]
-  set atKey (String? s) => atKeyObj.key = s;
-
-  /// See [AtKey.sharedWith]
-  String? get sharedWith => atKeyObj.sharedWith;
-  /// See [AtKey.sharedWith]
-  set sharedWith (String? s) => atKeyObj.sharedWith = VerbUtil.formatAtSign(s);
-
-  /// See [AtKey.sharedBy]
-  String? get sharedBy => atKeyObj.sharedBy;
-  /// See [AtKey.sharedBy]
-  set sharedBy (String? s) => atKeyObj.sharedBy = VerbUtil.formatAtSign(s);
 
   /// See [AtKey.isLocal]
   bool get isLocal => atKeyObj.isLocal;
@@ -71,94 +52,6 @@ class UpdateVerbBuilder extends AbstractVerbBuilder {
   String? operation;
 
   bool isJson = false;
-
-  @visibleForTesting
-  Metadata get metadata => atKeyObj.metadata!;
-
-  /// See [Metadata.isPublic]
-  bool get isPublic => metadata.isPublic!;
-  /// See [Metadata.isPublic]
-  set isPublic (bool b) => metadata.isPublic = b;
-
-  /// See [Metadata.isBinary]
-  bool? get isBinary => metadata.isBinary;
-  /// See [Metadata.isBinary]
-  set isBinary (bool? b) => metadata.isBinary = b;
-
-  /// See [Metadata.isEncrypted]
-  bool? get isEncrypted => metadata.isEncrypted;
-  /// See [Metadata.isEncrypted]
-  set isEncrypted (bool? b) => metadata.isEncrypted = b;
-
-  /// See [Metadata.ttl]
-  int? get ttl => metadata.ttl;
-  /// See [Metadata.ttl]
-  set ttl (int? i) => metadata.ttl = i;
-
-  /// See [Metadata.ttb]
-  int? get ttb => metadata.ttb;
-  /// See [Metadata.ttb]
-  set ttb (int? i) => metadata.ttb = i;
-
-  /// See [Metadata.ttr]
-  int? get ttr => metadata.ttr;
-  /// See [Metadata.ttr]
-  set ttr (int? i) => metadata.ttr = i;
-
-  /// See [Metadata.ccd]
-  bool? get ccd => metadata.ccd;
-  /// See [Metadata.ccd]
-  set ccd (bool? b) => metadata.ccd = b;
-
-  /// See [Metadata.dataSignature]
-  String? get dataSignature => metadata.dataSignature;
-  /// See [Metadata.dataSignature]
-  set dataSignature (String? s) => metadata.dataSignature = s;
-
-  /// See [Metadata.sharedKeyStatus]
-  String? get sharedKeyStatus => metadata.sharedKeyStatus;
-  /// See [Metadata.sharedKeyStatus]
-  set sharedKeyStatus (String? s) => metadata.sharedKeyStatus = s;
-
-  /// See [Metadata.sharedKeyEnc]
-  String? get sharedKeyEncrypted => metadata.sharedKeyEnc;
-  /// See [Metadata.sharedKeyEnc]
-  set sharedKeyEncrypted (String? s) => metadata.sharedKeyEnc = s;
-
-  /// See [Metadata.pubKeyCS]
-  String? get pubKeyChecksum => metadata.pubKeyCS;
-  /// See [Metadata.pubKeyCS]
-  set pubKeyChecksum (String? s) => metadata.pubKeyCS = s;
-
-  /// See [Metadata.encoding]
-  String? get encoding => metadata.encoding;
-  /// See [Metadata.encoding]
-  set encoding (String? s) => metadata.encoding = s;
-
-  /// See [Metadata.encKeyName]
-  String? get encKeyName => metadata.encKeyName;
-  /// See [Metadata.encKeyName]
-  set encKeyName (String? s) => metadata.encKeyName = s;
-
-  /// See [Metadata.encAlgo]
-  String? get encAlgo => metadata.encAlgo;
-  /// See [Metadata.encAlgo]
-  set encAlgo (String? s) => metadata.encAlgo = s;
-
-  /// See [Metadata.ivNonce]
-  String? get ivNonce => metadata.ivNonce;
-  /// See [Metadata.ivNonce]
-  set ivNonce (String? s) => metadata.ivNonce = s;
-
-  /// See [Metadata.skeEncKeyName]
-  String? get skeEncKeyName => metadata.skeEncKeyName;
-  /// See [Metadata.skeEncKeyName]
-  set skeEncKeyName (String? s) => metadata.skeEncKeyName = s;
-
-  /// See [Metadata.skeEncAlgo]
-  String? get skeEncAlgo => metadata.skeEncAlgo;
-  /// See [Metadata.skeEncAlgo]
-  set skeEncAlgo (String? s) => metadata.skeEncAlgo = s;
 
   @override
   String buildCommand() {
