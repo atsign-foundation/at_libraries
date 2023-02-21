2833fd9ca409f2748eb824ca7a0c9c71e35f2645
murali-shris <murali.25@gmail.com>
2023-02-21 13:54:18 +0530

    fix: added tests for ecc signing algo

--- packages/at_chops/lib/src/algorithm/ecc_signing_algo.dart
+++ packages/at_chops/lib/src/algorithm/ecc_signing_algo.dart
@@ -5,29 +5,45 @@ import 'package:at_chops/src/algorithm/at_algorithm.dart';
 import 'package:at_commons/at_commons.dart';
 import 'package:crypto/crypto.dart';
 import 'package:ecdsa/ecdsa.dart' as ecdsa;
-import 'package:elliptic/elliptic.dart';
+import 'package:elliptic/elliptic.dart' as elliptic;
 
 /// Data signing and verification for ECDSA - elliptic curve
+/// Currently uses sha256 hashing for data. #TODO implement using _hashingAlgoType in the future.
 class EccSigningAlgo implements AtSigningAlgorithm {
   SigningAlgoType? _signingAlgoType;
   HashingAlgoType? _hashingAlgoType;
+  elliptic.PrivateKey? _privateKey;
+
   EccSigningAlgo();
 
   @override
-  Uint8List sign(Uint8List dataHash) {
-    throw UnimplementedError('not implemented');
+  Uint8List sign(Uint8List data) {
+    if (_privateKey == null) {
+      throw AtException(
+          'elliptic private key has to be set for signing operation');
+    }
+    var hashHex = sha256.convert(data).toString();
+    var hash = List<int>.generate(hashHex.length ~/ 2,
+        (i) => int.parse(hashHex.substring(i * 2, i * 2 + 2), radix: 16));
+    return Uint8List.fromList(
+        ecdsa.signature(_privateKey!, hash).toCompactHex().codeUnits);
   }
 
   @override
   bool verify(Uint8List signedData, Uint8List signature, {String? publicKey}) {
-    if (publicKey == null) {
+    if (publicKey == null && _privateKey == null) {
       throw AtException('public key not set not ecc verification');
     }
-    var ec = getSecp256r1();
+    var ec = elliptic.getSecp256r1();
     var hashHex = sha256.convert(signedData).toString();
     var hash = List<int>.generate(hashHex.length ~/ 2,
         (i) => int.parse(hashHex.substring(i * 2, i * 2 + 2), radix: 16));
-    var pubKey = ec.hexToPublicKey(publicKey);
+    var pubKey;
+    if (publicKey != null) {
+      pubKey = ec.hexToPublicKey(publicKey);
+    } else {
+      pubKey = _privateKey!.publicKey;
+    }
     var eccSignature =
         ecdsa.Signature.fromCompactHex(String.fromCharCodes(signature));
     return ecdsa.verify(pubKey, hash, eccSignature);
@@ -38,7 +54,7 @@ class EccSigningAlgo implements AtSigningAlgorithm {
 
   @override
   void setHashingAlgoType(HashingAlgoType? hashingAlgoType) {
-    // TODO: implement setHashingAlgoType
+    _hashingAlgoType = hashingAlgoType;
   }
 
   @override
@@ -46,6 +62,12 @@ class EccSigningAlgo implements AtSigningAlgorithm {
 
   @override
   void setSigningAlgoType(SigningAlgoType? signingAlgoType) {
-    // TODO: implement setSigningAlgoType
+    _signingAlgoType = signingAlgoType;
+  }
+
+  elliptic.PrivateKey? get privateKey => _privateKey;
+
+  set privateKey(elliptic.PrivateKey? value) {
+    _privateKey = value;
   }
 }
---
+++ packages/at_chops/test/ecc_signing_algo_test.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:at_chops/at_chops.dart';
+import 'package:at_chops/src/algorithm/ecc_signing_algo.dart';
+import 'package:ecdsa/ecdsa.dart';
+import 'package:elliptic/elliptic.dart';
+import 'package:at_chops/src/algorithm/at_algorithm.dart';
+import 'package:at_chops/src/algorithm/default_signing_algo.dart';
+import 'package:at_commons/at_commons.dart';
+import 'package:at_utils/at_logger.dart';
+import 'package:crypton/crypton.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests for ecc signing and verification', () {
+    test(
+        'Test ecc signing and verification using generated private key from ecc',
+        () {
+      final eccAlgo = EccSigningAlgo();
+      var ec = getSecp256r1();
+      final eccPrivateKey = ec.generatePrivateKey();
+      eccAlgo.privateKey = eccPrivateKey;
+      final dataToSign = 'Hello World';
+      final dataInBytes = Uint8List.fromList(dataToSign.codeUnits);
+      final signature = eccAlgo.sign(dataInBytes);
+      var verifyResult = eccAlgo.verify(dataInBytes, signature);
+      expect(verifyResult, true);
+    });
+    test('Test ecc verification by passing public key', () {
+      final eccAlgo = EccSigningAlgo();
+      var ec = getSecp256r1();
+      final eccPrivateKey = ec.generatePrivateKey();
+      eccAlgo.privateKey = eccPrivateKey;
+      final publicKey = eccPrivateKey.publicKey;
+      final dataToSign = 'Hello World';
+      final dataInBytes = Uint8List.fromList(dataToSign.codeUnits);
+      final signature = eccAlgo.sign(dataInBytes);
+      var verifyResult = eccAlgo.verify(dataInBytes, signature,
+          publicKey: publicKey.toString());
+      expect(verifyResult, true);
+    });
+    test('Test ecc signing - private key not set', () {
+      final eccAlgo = EccSigningAlgo();
+      final dataToSign = 'Hello World';
+      final dataInBytes = Uint8List.fromList(dataToSign.codeUnits);
+      expect(
+          () => eccAlgo.sign(dataInBytes),
+          throwsA(predicate((e) =>
+              e is AtException &&
+              e.toString().contains(
+                  'elliptic private key has to be set for signing operation'))));
+    });
+    test('Test ecc verification - private key and public key not available',
+        () {
+      final eccAlgo = EccSigningAlgo();
+      var ec = getSecp256r1();
+      final eccPrivateKey = ec.generatePrivateKey();
+      eccAlgo.privateKey = eccPrivateKey;
+      final dataToSign = 'Hello World';
+      final dataInBytes = Uint8List.fromList(dataToSign.codeUnits);
+      final signature = eccAlgo.sign(dataInBytes);
+      eccAlgo.privateKey = null;
+      expect(
+          () => eccAlgo.verify(dataInBytes, signature),
+          throwsA(predicate((e) =>
+              e is AtException &&
+              e
+                  .toString()
+                  .contains('public key not set not ecc verification'))));
+    });
+  });
+}
