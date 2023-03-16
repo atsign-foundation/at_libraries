d9d88ef4c1fd3e14a1d52f2897551a01d320e639
srieteja <srieteja@atsign.com>
2023-03-16 11:34:13 +0530

    chore: remove deprecated methods usage from example file

--- example/main.dart
+++ example/main.dart
@@ -5,8 +5,6 @@ void main(List<String> arguments) {
   var atSign = '@alice';
   var fixedAtSign = AtUtils.fixAtSign(atSign);
   logger.info(fixedAtSign);
-  var formattedAtSign = AtUtils.formatAtSign(atSign);
-  logger.finer(formattedAtSign);
   var atSignSha = AtUtils.getShaForAtSign(atSign);
   logger.finest(atSignSha);
 }
