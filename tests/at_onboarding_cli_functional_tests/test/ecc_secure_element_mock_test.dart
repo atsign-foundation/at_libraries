f6541d29cf8b0141d6ea7f1b9125d77b9e75e17c
purnimavenkatasubbu <svpurnima94@gmail.com>
2023-03-28 15:54:14 +0530

    modified tests

--- tests/at_onboarding_cli_functional_tests/test/ecc_secure_element_mock_test.dart
+++ tests/at_onboarding_cli_functional_tests/test/ecc_secure_element_mock_test.dart
@@ -1,15 +1,62 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:at_chops/at_chops.dart';
+import 'package:at_client/at_client.dart';
 import 'package:at_onboarding_cli/at_onboarding_cli.dart';
 import 'package:at_utils/at_logger.dart';
-
+import 'package:test/test.dart';
+import 'package:at_demo_data/at_demo_data.dart' as at_demos;
 import 'at_chops_secure_element_mock.dart';
-
 
 /// Usage: dart main.dart <cram_secret>
 Future<void> main() async {
-  final atSign = '@alice';
+  AtSignLogger.root_level = 'FINER';
+  var logger = AtSignLogger('OnboardSecureElement');
 
-  AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
+  final atSign = '@bob🛠';
+  test('Test auth functionality', () async {
+    AtOnboardingPreference preference = getPreferences(atSign);
+    AtOnboardingService onboardingService =
+        AtOnboardingServiceImpl(atSign, preference);
+    // create empty keys in atchops. Encryption key pair will be set later on after generation
+    final atChopsImpl =
+        AtChopsSecureElementMock(AtChopsKeys.create(null, null));
+    onboardingService.atChops = atChopsImpl;
+    atChopsImpl.init();
+    logger.info('calling onboard');
+    bool isOnboarded = await onboardingService.onboard();
+    expect(isOnboarded, true);
+    logger.info('onboard done');
+    logger.info('calling auth');
+
+    bool status = await onboardingService.authenticate();
+    logger.info('auth done');
+    expect(status, true);
+
+    // update a key
+    AtClient? atClient = await onboardingService.getAtClient();
+    await insertSelfEncKey(atClient, atSign,
+        selfEncryptionKey:
+            await getSelfEncryptionKey(preference.atKeysFilePath!));
+    AtKey key = AtKey();
+    key.key = 'securedKey';
+    key.namespace = 'wavi';
+    var putResult = await atClient?.put(key, 'securedvalue');
+    expect(putResult, true);
+    AtValue? response = await atClient?.get(key);
+    expect('securedvalue', response?.value);
+    var deleteResponse = await atClient?.delete(key);
+    expect(deleteResponse, true);
+  });
+
+  tearDown(() async {
+    await tearDownFunc();
+  });
+}
+
+AtOnboardingPreference getPreferences(String atsign) {
+  AtOnboardingPreference atOnboardingPreference = AtOnboardingPreference()
     ..hiveStoragePath =
         'storage/hive'
     ..namespace = 'wavi'
@@ -20,28 +67,38 @@ Future<void> main() async {
         'storage/commitLog'
     ..rootDomain = 'vip.ve.atsign.zone'
     ..fetchOfflineNotifications = true
-    ..atKeysFilePath = 'storage/files/@alice_key.atKeys'
+    ..atKeysFilePath =
+        'storage/files/@bob🛠_key.atKeys'
     ..useAtChops = true
     ..signingAlgoType = SigningAlgoType.ecc_secp256r1
     ..hashingAlgoType = HashingAlgoType.sha256
     ..authMode = PkamAuthMode.sim
     ..publicKeyId = '3023020'
-    ..cramSecret =
-        'b26455a907582760ebf35bc4847de549bc41c24b25c8b1c58d5964f7b4f8a43bc55b0e9a601c9a9657d9a8b8bbc32f88b4e38ffaca03c8710ebae1b14ca9f364'
+    ..cramSecret = at_demos.cramKeyMap[atsign]
     ..skipSync = true;
-  AtSignLogger.root_level = 'FINER';
-  var logger = AtSignLogger('OnboardSecureElement');
 
-  AtOnboardingService onboardingService =
-  AtOnboardingServiceImpl(atSign, atOnboardingConfig);
-  // create empty keys in atchops. Encryption key pair will be set later on after generation
-  final atChopsImpl = AtChopsSecureElementMock(AtChopsKeys.create(null, null));
-  onboardingService.atChops = atChopsImpl;
-  atChopsImpl.init();
-  logger.info('calling onboard');
-  await onboardingService.onboard();
-  logger.info('onboard done');
-  logger.info('calling auth');
-  await onboardingService.authenticate();
-  logger.info('auth done');
+  return atOnboardingPreference;
+}
+
+Future<String?> getSelfEncryptionKey(String atKeysFilePath) async {
+  String atAuthData = await File(atKeysFilePath).readAsString();
+  Map<String, String> jsonData = <String, String>{};
+  json.decode(atAuthData).forEach((String key, dynamic value) {
+    jsonData[key] = value.toString();
+  });
+  return jsonData[AT_ENCRYPTION_SELF_KEY];
+}
+
+Future<void> insertSelfEncKey(AtClient? atClient, String atsign,
+    {String? selfEncryptionKey}) async {
+  await atClient?.getLocalSecondary()?.putValue(
+      AT_ENCRYPTION_SELF_KEY, selfEncryptionKey ?? at_demos.aesKeyMap[atsign]!);
+  return;
+}
+
+Future<void> tearDownFunc() async {
+  bool isExists = await Directory('storage/').exists();
+  if (isExists) {
+    Directory('storage/').deleteSync(recursive: true);
+  }
 }
