fe2859ca15f6913cadd04a6ebe3ceb9fe3cc31b3
murali-shris <murali.25@gmail.com>
2023-04-06 17:34:34 +0530

    fix: remove pkam auth mode and use the enum from at_commons

--- packages/at_onboarding_cli/lib/src/util/at_onboarding_preference.dart
+++ packages/at_onboarding_cli/lib/src/util/at_onboarding_preference.dart
@@ -2,6 +2,7 @@ import 'dart:core';
 
 import 'package:at_chops/at_chops.dart';
 import 'package:at_client/at_client.dart';
+import 'package:at_commons/at_commons.dart';
 
 class AtOnboardingPreference extends AtClientPreference {
   //specify path of .atKeysFile containing encryption keys
@@ -23,8 +24,3 @@ class AtOnboardingPreference extends AtClientPreference {
 
   bool skipSync = false;
 }
-
-// If pkam auth mode is keysFile then pkam private key will be generated during onboarding and saved in the keys file.
-// For subsequent authentication, pkam private key will be read from the keys file supplied by the user.
-// If pkam auth mode is sim or any other secure element, then private key is not accessible directly. Only the data will be passed to the sim/secure element, pkam signature can be retrieved and verified.pkam private key will not be a part of keys file in this case.
-enum PkamAuthMode { keysFile, sim }
