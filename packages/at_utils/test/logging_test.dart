df31c9d14141d824b09e1549b339aded19c7cc03
gkc <gary.casey@gmail.com>
2023-07-18 14:37:07 +0100

    fix: use the defaultLoggingHandler if none is supplied test: add a couple of unit tests for AtSignLogger and default logging handler

--- packages/at_utils/test/logging_test.dart
+++ packages/at_utils/test/logging_test.dart
@@ -4,22 +4,71 @@ import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 void main() {
+  setUp(() {
+    AtSignLogger.defaultLoggingHandler = AtSignLogger.consoleLoggingHandler;
+  });
+
   group('A group of fixAtSign tests', () {
-    test('Test file logging', () => testConsoleLogging());
+    test('Test logging handler', () {
+      var records = <LogRecord>[];
+      var testLogger = AtSignLogger('test_console_logging');
+      testLogger.logger.onRecord.listen(records.add);
+      testLogger.info('hello');
+      expect(records[0].message, 'hello');
+    });
+
+    test('Test default logging handler', () {
+      var lh1 = MyLoggingHandler(AtSignLogger.consoleLoggingHandler);
+      AtSignLogger.defaultLoggingHandler = lh1;
+      var l1 = AtSignLogger('console');
+
+      l1.info('testing 1');
+      expect(lh1.lastLogRecord!.message, 'testing 1');
+
+      var lh2 = MyLoggingHandler(AtSignLogger.stdErrLoggingHandler);
+      AtSignLogger.defaultLoggingHandler = lh2;
+      var l2 = AtSignLogger('stderr');
+      l2.info('testing 2');
+      expect(lh2.lastLogRecord!.message, 'testing 2');
+    });
+
+    test('Test override logging handler for instance', () {
+      var testDefaultLH = MyLoggingHandler(ConsoleLoggingHandler());
+      AtSignLogger.defaultLoggingHandler = testDefaultLH;
+
+      expect(AtSignLogger.defaultLoggingHandler, testDefaultLH);
+      var lh1 = NullLoggingHandler();
+      var l1 = AtSignLogger('null handler', loggingHandler: lh1);
+
+      expect(AtSignLogger.defaultLoggingHandler, testDefaultLH);
+      var l2 = AtSignLogger('console'); // should use the defaultLoggingHandler
+
+      l1.info('testing per-instance logging handler');
+      expect(lh1.lastLogRecord!.message, 'testing per-instance logging handler');
+      expect(testDefaultLH.lastLogRecord, null);
+
+      l2.info('testing with default logging handler');
+      expect(lh1.lastLogRecord!.message, 'testing per-instance logging handler');
+      expect(testDefaultLH.lastLogRecord!.message, 'testing with default logging handler');
+    });
   });
 }
 
-void deleteFile(filename) {
-  var file = File(filename);
-  if (file.existsSync()) {
-    file.deleteSync();
+class MyLoggingHandler implements LoggingHandler {
+  final LoggingHandler delegate;
+  MyLoggingHandler(this.delegate);
+  LogRecord? lastLogRecord;
+  @override
+  void call(LogRecord record) {
+    lastLogRecord = record;
+    delegate.call(record);
   }
 }
 
-void testConsoleLogging() {
-  var records = <LogRecord>[];
-  var testLogger = AtSignLogger('test_console_logging');
-  testLogger.logger.onRecord.listen(records.add);
-  testLogger.info('hello');
-  expect(records[0].message, 'hello');
+class NullLoggingHandler implements LoggingHandler {
+  LogRecord? lastLogRecord;
+  @override
+  void call(LogRecord record) {
+    lastLogRecord = record;
+  }
 }
