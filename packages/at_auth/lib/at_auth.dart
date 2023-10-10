a234f55830f2da330fcdcc5478432a62eb3e5fcf
murali-shris <murali.25@gmail.com>
2023-10-10 15:18:30 +0530

    fix: export auth request and response

--- packages/at_auth/lib/at_auth.dart
+++ packages/at_auth/lib/at_auth.dart
@@ -4,4 +4,6 @@ export 'src/at_auth_base.dart';
 export 'src/at_auth_impl.dart';
 export 'src/onboard/at_onboarding_request.dart';
 export 'src/onboard/at_onboarding_response.dart';
+export 'src/auth/at_auth_request.dart';
+export 'src/auth/at_auth_response.dart';
 export 'src/keys/at_auth_keys.dart';
