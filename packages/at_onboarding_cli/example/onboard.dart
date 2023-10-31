06db20b2cd47e02fee850c85cd4002eb26e8fd33
Srie Teja <srit1505@gmail.com>
2023-10-31 14:07:49 +0530

    fix: remove usage of a default cram key

--- packages/at_onboarding_cli/example/onboard.dart
+++ packages/at_onboarding_cli/example/onboard.dart
@@ -8,17 +8,13 @@ import 'util/custom_arg_parser.dart';
 Future<void> main(List<String> args) async {
   AtSignLogger.root_level = 'finest';
 
-  ///ToDo: remove before push
-  String demoCramKey =
-      'b26455a907582760ebf35bc4847de549bc41c24b25c8b1c58d5964f7b4f8a43bc55b0e9a601c9a9657d9a8b8bbc32f88b4e38ffaca03c8710ebae1b14ca9f364';
-
   final argResults = CustomArgParser(getArgParser()).parse(args);
 
   final atSign = argResults['atsign'];
   AtOnboardingPreference atOnboardingPreference = AtOnboardingPreference()
     ..namespace =
         'wavi' // unique identifier that can be used to identify data from your app
-    ..cramSecret = demoCramKey //argResults['cram']
+    ..cramSecret = argResults['cram']
     ..atKeysFilePath = argResults['atKeysPath']
     ..appName = 'wavi'
     ..deviceName = 'pixel'
