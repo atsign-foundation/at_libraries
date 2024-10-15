f46bbf9e5bf2efed00fc9ff4b00ac5dfa55cd7e2
Murali <muralidharan@shrisinfotech.com>
2024-10-15 11:47:42 +0530

    fix: export at_key_regex utils

--- packages/at_commons/lib/at_commons.dart
+++ packages/at_commons/lib/at_commons.dart
@@ -29,6 +29,7 @@ export 'package:at_commons/src/verb/verb_util.dart';
 export 'package:at_commons/src/auth/auth_mode.dart';
 export 'package:at_commons/src/verb/enroll_params.dart';
 export 'package:at_commons/src/enroll/enrollment.dart';
+export 'package:at_commons/src/utils/at_key_regex_utils.dart';
 @experimental
 export 'package:at_commons/src/telemetry/at_telemetry.dart';
 export 'package:at_commons/src/utils/string_utils.dart';
